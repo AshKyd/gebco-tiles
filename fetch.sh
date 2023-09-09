0eb0d7da377b611e47a46dd5d740a97ae338e34e
Ash Kyd <ash@kyd.com.au>
2023-09-09 14:56:46 +1000

    fix fetch, tweak process

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf data
+mkdir data
 cd data
 # The server is kinda slow, this lets us use 16 connections
 aria2c -x 16 https://www.bodc.ac.uk/data/open_download/gebco/gebco_2023/geotiff/
